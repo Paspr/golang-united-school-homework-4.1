a635eb299f828ba4575c921ffa32b070495b19ce
Pavel Osminin <osperevod@gmail.com>
2022-07-09 23:56:37 +0500

    initial version of 'reverse-string' task

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,17 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	tempRune := []rune(input)
+	tempRuneLen := len(tempRune)
+	newRune := make([]rune, tempRuneLen)
+
+	// iterate tempRune backwards and
+	// assign the last symbols of tempRune to
+	// initial symbols of empty newRune
+	for i := tempRuneLen - 1; i >= 0; i-- {
+		newRune[tempRuneLen-i-1] = tempRune[i]
+	}
+	output = string(newRune)
+
 	return output
 }
